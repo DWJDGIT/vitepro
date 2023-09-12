f8ff52c8c2652537177301f311568b85579cd392
YDW <2310861314@qq.com>
2023-09-12 16:46:41 +0800

    🚀 新增(view):

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,11 +2,11 @@
  * @Author       : 'YDW'
  * @Date         : 2023-09-12 16:09:09
  * @LastEditors  : 'YDW' 2310861314@qq.com
- * @LastEditTime : 2023-09-12 16:09:15
+ * @LastEditTime : 2023-09-12 16:46:29
  * @Description  : 
 -->
 <template>
-  <div class="main">主页面</div>
+  <div class="main">页面组合</div>
 </template>
 
 <script setup lang="ts"></script>
